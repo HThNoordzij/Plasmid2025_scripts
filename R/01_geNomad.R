b79129424772c8b2c1f13ec51dc9356af5ac0412
Hanna Noordzij <44413749+HThNoordzij@users.noreply.github.com>
2025-09-03 11:26:56 +0200

    change to fread and add viral contigs

--- R/01_geNomad.R
+++ R/01_geNomad.R
@@ -4,6 +4,7 @@ rm(list=ls(all=TRUE))
 #Load package
 library("tidyverse")
 library("Biostrings")
+library(data.table)
 
 # set working directory
 setwd("~/PATH/")
@@ -17,24 +18,34 @@ get_ID <- function(x) {
 
 ###############################   geNomad  #####################################
 ### Load geNomad file of metaplasmidSPAdes contigs
-file_genomad_spades <- "PATH/plasmid_summary.tsv"
-df_genomad_spades <- read_tsv(file_genomad_spades)
-df_genomad <- df_genomad_spades %>%
+file_genomad_plasmid <- "geNomad/plasmid_summary.tsv"
+df_genomad_plasmid <- fread(file_genomad_plasmid, 
+                            sep = "\t",
+                            fill = TRUE)
+df_genomad <- df_genomad_plasmid %>%
   mutate(child = sapply(strsplit(seq_name, "_"), get_ID))
 
+## count viral contigs
+### Load geNomad SPAdes
+file_genomad_virus <- "geNomad/all_basic_scaffolds_virus_summary.tsv"
+df_genomad_virus <- fread(file_genomad_virus, 
+                          sep = "\t",
+                          fill = TRUE)
+df_genomad_virus <- df_genomad_virus %>%
+  mutate(child = sapply(strsplit(seq_name, "_"), get_ID))
 
 ###############################    fasta   #####################################
 ### Load contigs of metaplasmidSPAdes
-file_contigs_spades <- "PATH/scaffolds.fasta"
-df_contigs_spades <- readDNAStringSet(file_contigs_spades)
+file_contigs_plasmid <- "scaffolds/scaffolds.fasta"
+df_contigs_plasmid <- readDNAStringSet(file_contigs_plasmid)
 
 ### select reference sequences
-df_contigs_spades_filter <- extractList(df_contigs_spades, df_genomad$seq_name)
-df_contigs_spades_filter <- unlist(df_contigs_spades_filter)
+df_contigs_plasmid_filter <- extractList(df_contigs_plasmid, df_genomad$seq_name)
+df_contigs_plasmid_filter <- unlist(df_contigs_plasmid_filter)
 
 # Save potential plasmids as fasta
 file_contigs_bins <- "scaffolds/scaffolds_geNomad.fasta"
-writeXStringSet(df_contigs_spades_filter ,file_contigs_bins)
+writeXStringSet(df_contigs_plasmid_filter ,file_contigs_bins)
 
 # Save circular file for MobMess
 file_mobmess_circ <- "MobMess/circular_scaffolds_geNomad.txt"
@@ -47,14 +58,14 @@ df_mobless_circ <- df_genomad %>%
 write.table(df_mobless_circ, file = file_mobmess_circ,
             sep = "\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
-### split per child
+### split fasta files per child
 for (i in 1:12) {
   tmp_child <- paste("id", i, sep = "")
   
   # split fasta
-  row_filter <- extractList(df_contigs_spades_filter, 
-                            names(df_contigs_spades_filter)[grepl(paste0(tmp_child, "_"), 
-                                                                  names(df_contigs_spades_filter))])
+  row_filter <- extractList(df_contigs_plasmid_filter, 
+                            names(df_contigs_plasmid_filter)[grepl(paste0(tmp_child, "_"), 
+                                                                  names(df_contigs_plasmid_filter))])
   row_filter <- unlist(row_filter)
   names(row_filter) <- gsub("_", ".", names(row_filter))
   row_filter <- row_filter %>% as.character()
